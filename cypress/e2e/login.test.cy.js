6863fceb492fc8d336d53c8d7148ba838ca533fe
Wardatunnisa <Wardatunnisa2001@gmail.com>
2023-02-19 20:50:53 +0700

    update tugas login

---
+++ cypress/e2e/login.test.cy.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress"/>
+
+describe('Login/Logout Test', () => {
+    before('', () => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include', 'index.html')
+        cy.get('#signin_button').click();
+    });
+
+    it('Should try to login with invalid data', () => {
+        cy.get('#user_login').should('be.visible')
+        cy.get('#user_password').should('be.visible')
+        cy.get('#user_login').type('invalid username')
+        cy.get('#user_password').type('invalid passowrd')
+        cy.get('#user_remember_me').click()
+        cy.get('.btn').click()
+    });
+
+    it('Should display error message', () => {
+        cy.get('.alert-error').should('be.visible').and('contain.text', 'Login and/or password are wrong.')
+    });
+
+    it('Should login to application with valid data', () => {
+        cy.fixture('user').then(user => {
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user_login').clear()
+            cy.get('#user_login').type(username)
+            cy.get('#user_password').clear()
+            cy.get('#user_password').type(password)
+            cy.get('#user_remember_me').click()
+            cy.get('.btn').click()
+
+            cy.get('h2').should('contain.text', 'Cash Accounts')
+
+
+        })
+    });
+
+    it('Should logout from the application', () => {
+        cy.get(':nth-child(3) > .dropdown-toggle').click()
+        cy.get('#logout_link').click()
+
+        cy.get('strong').should('contain.text', 'Online Banking')
+
+    });
+
+});
